ce048edcb5072b5ef45217807cd194acbc9638b9
jacyarapontes <ana.jacyara2014@gmail.com>
2022-11-28 11:13:20 -0300

    Adicionando imagem

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React, {Component} from 'react';
 class App extends Component{
     render(){
     return(
-        <div>
-        123
+        <div className='container'>
+        <img alt='cronometro' src={require('./assets/cronometro.png')}className='img'/><br/>
+
+        <a className='timer'>0.0</a><br/>
+        
+        <div className='areaBtn'>
+            <a className='botao'>VAI</a><br/>
+            <a className='botao'>LIMPAR</a><br/>
+        </div>
         </div>
 
     );
